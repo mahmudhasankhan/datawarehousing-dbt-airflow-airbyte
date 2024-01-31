b6960dc5c9f061836c58ab61ac44a6c1d10f3892
mahmudhasankhan <khanzadeh008@gmail.com>
2024-01-31 18:52:46 +0600

    Started writing batch processing script

--- ELT/elt/elt_script.py
+++ ELT/elt/elt_script.py
@@ -3,8 +3,10 @@
 import subprocess
 import time
 
+# Checking if postgres is ready
 
-def wait_for_postgres(host, max_retries=5, delay_seconds=5):
+
+def wait_for_postgres(host, max_retries=5, delay_seconds=5) -> bool:
     retries = 0
     while retries < max_retries:
         try:
@@ -26,3 +28,33 @@ def wait_for_postgres(host, max_retries=5, delay_seconds=5):
             time.sleep(delay_seconds)
     print("Max retries reached. Exiting")
     return False
+
+
+if not wait_for_postgres(host="source_postgres"):
+    exit(1)
+
+print("Starting ELT Script...")
+
+source_config = {
+    'dbname': 'source_db',
+    'user': 'postgres',
+    'password': 'secret',
+    "host": "source_postgres"
+}
+destination_config = {
+    "dbname": "destination_db",
+    "user": "postgres",
+    "password": "secret",
+    "host": "destination_postgres"
+}
+
+dump_command = [
+    "pg_dump",
+    "-h", source_config['host'],
+    "-u", source_config['user'],
+    "-d", source_config['dbname'],
+    "-f", "data_dump.sql",
+    "-w"
+]
+
+subprocess_env = dict(PGPASSWORD=source_config["password"])
