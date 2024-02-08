56190de540bdd4f21b89de20f3d572828907b034
mahmudhasankhan <khanzadeh008@gmail.com>
2024-02-08 17:34:47 +0600

    start and stop shell scripts for automation

--- ELT/start.sh
+++ ELT/start.sh
@@ -1,3 +1,14 @@
-cron &
+docker compose up -d
 
-python /app/elt_script.py
+sleep 5
+
+cd airbyte
+
+# Check if docker-compose.yml exists in the current directory
+if [ -f "docker-compose.yaml" ]; then
+  # If it exists, run docker-compose up
+  docker-compose up -d
+else
+  # Otherwise, run the setup script
+  ./run-ab-platform.sh
+fi
